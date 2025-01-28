77bfc8fcce693c11f0e1bb9e7ed64ae570a8a898
herc <101748155+mouxmoux@users.noreply.github.com>
2025-01-28 14:45:56 +0200

    added school & department mapping

--- src/main/java/gr/eduping/eduping/mapper/InstitutionMapper.java
+++ src/main/java/gr/eduping/eduping/mapper/InstitutionMapper.java
@@ -1,7 +1,13 @@
 package gr.eduping.eduping.mapper;
 
+import gr.eduping.eduping.dto.DepartmentReadOnlyDTO;
 import gr.eduping.eduping.dto.InstitutionReadOnlyDTO;
+import gr.eduping.eduping.dto.SchoolReadOnlyDTO;
+import gr.eduping.eduping.model.static_data.Country;
+import gr.eduping.eduping.model.static_data.Department;
 import gr.eduping.eduping.model.static_data.Institution;
+import gr.eduping.eduping.model.static_data.School;
+import gr.eduping.eduping.repository.CountryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -9,10 +15,32 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class InstitutionMapper {
 
+    private final CountryRepository countryRepository;
+
     public InstitutionReadOnlyDTO mapToInstitutionReadOnlyDTO(Institution institution) {
         InstitutionReadOnlyDTO institutionReadOnlyDTO = new InstitutionReadOnlyDTO();
         institutionReadOnlyDTO.setName(institution.getName());
-
+        // TODO issue when trying to handle exception for below
+        if (countryRepository.findByCityId(institution.getCity().getId()).isPresent()) {
+            Country country = countryRepository.findByCityId(institution.getCity().getId()).get();
+            institutionReadOnlyDTO.setCountry(country.getName());
+        }
         return institutionReadOnlyDTO;
+    }
+
+    public SchoolReadOnlyDTO mapToSchoolReadOnlyDTO(School school) {
+        SchoolReadOnlyDTO schoolReadOnlyDTO = new SchoolReadOnlyDTO();
+        schoolReadOnlyDTO.setSchool(school.getName());
+        schoolReadOnlyDTO.setInstitution(school.getInstitution().getName());
+        return schoolReadOnlyDTO;
+    }
+
+    public DepartmentReadOnlyDTO mapToDepartmentReadOnlyDTO(Department department) {
+        DepartmentReadOnlyDTO departmentReadOnlyDTO = new DepartmentReadOnlyDTO();
+        departmentReadOnlyDTO.setDepartment(department.getName());
+        departmentReadOnlyDTO.setCity(department.getCity().getName());
+        departmentReadOnlyDTO.setSchool(department.getSchool().getName());
+        departmentReadOnlyDTO.setInstitution(department.getSchool().getInstitution().getName());
+        return departmentReadOnlyDTO;
     }
 }
