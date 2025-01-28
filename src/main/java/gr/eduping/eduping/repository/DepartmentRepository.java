5dc8b95820c2bff329d7b87f6606d565daa21c2d
herc <101748155+mouxmoux@users.noreply.github.com>
2025-01-28 12:54:18 +0200

    department derived query

--- src/main/java/gr/eduping/eduping/repository/DepartmentRepository.java
+++ src/main/java/gr/eduping/eduping/repository/DepartmentRepository.java
@@ -4,5 +4,8 @@ import gr.eduping.eduping.model.static_data.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Set;
+
 public interface DepartmentRepository extends JpaRepository<Department, Long>, JpaSpecificationExecutor<Long> {
+    Set<Department> findAllByUsersId(Long id);
 }
