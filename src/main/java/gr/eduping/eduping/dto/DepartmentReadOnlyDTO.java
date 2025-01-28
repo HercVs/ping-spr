b5af4528dc36e9106770a2be697e1e4647473cba
herc <101748155+mouxmoux@users.noreply.github.com>
2025-01-28 12:36:52 +0200

    read only dtos

---
+++ src/main/java/gr/eduping/eduping/dto/DepartmentReadOnlyDTO.java
@@ -0,0 +1,17 @@
+package gr.eduping.eduping.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class DepartmentReadOnlyDTO {
+    private String department;
+    private String city;
+    private String school;
+    private String institution;
+}
---
+++ src/main/java/gr/eduping/eduping/dto/InstitutionReadOnlyDTO.java
@@ -0,0 +1,14 @@
+package gr.eduping.eduping.dto;
+
+import gr.eduping.eduping.model.static_data.Country;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class InstitutionReadOnlyDTO {
+
+    private String name;
+}
